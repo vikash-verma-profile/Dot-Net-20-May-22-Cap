1a882fe544ffc47d09e686623ab61c90be242222
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-05-20 17:07:52 +0530

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //<data-type>  <variable-name>
+            int a=5;
+            int b = 5;
+            //string name = "Vikash Verma";
+            //Console.WriteLine(name);
+            Console.Write(a+b);
+           // Console.Write(a);
+
+        }
+    }
+}
