89ed121fc02618ae6197447a23468fc320012018
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-05-23 09:47:04 +0530

    first commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,9 +12,21 @@ namespace ConsoleApp1
             int b = 5;
             //string name = "Vikash Verma";
             //Console.WriteLine(name);
-            Console.Write(a+b);
-           // Console.Write(a);
+            //Console.Write(a+b);
+            // Console.Write(a);
 
+
+            //whether a number is positive or not
+            int positiveNumber = -5;
+            Console.WriteLine();
+            if (positiveNumber > 0)
+            {
+                Console.WriteLine(positiveNumber +" is a positive Number");
+            }
+            else
+            {
+                Console.WriteLine(positiveNumber + " is not a positive Number");
+            }
         }
     }
 }
