2304c27cabcfe3b170318485f0d18565f000840f
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-05-24 17:47:04 +0530

    first commit

--- ConsoleApp1/ConsoleApp1/Class5.cs
+++ ConsoleApp1/ConsoleApp1/Class5.cs
@@ -23,22 +23,43 @@ namespace ConsoleApp1
                 }
             }
             //Input from arr2
+            //for (int row = 0; row < 2; row++)
+            //{
+            //    for (int col = 0; col < 2; col++)
+            //    {
+            //        Console.WriteLine("Please enter the value for arr2[" + row + "," + col + "]: ");
+            //        arr2[row, col] = Convert.ToInt32(Console.ReadLine());
+            //    }
+            //}
+
+            Console.WriteLine("Matrix before Transpose");
+            //Matrix before Transpose
             for (int row = 0; row < 2; row++)
             {
                 for (int col = 0; col < 2; col++)
                 {
-                    Console.WriteLine("Please enter the value for arr2[" + row + "," + col + "]: ");
-                    arr2[row, col] = Convert.ToInt32(Console.ReadLine());
+                    //Console.Write((arr1[row, col]+arr2[row,col])+"\t");
+                    Console.Write(arr1[row, col] + "\t");
                 }
+                Console.WriteLine();
             }
 
-
+            //process for transpose
+            for (int row = 0; row < 2; row++)
+            {
+                for (int col = 0; col < 2; col++)
+                {
+                    arr2[col, row] = arr1[row, col];
+                }
+            }
+            Console.WriteLine("Matrix After Transpose");
             //Output
             for (int row = 0; row < 2; row++)
             {
                 for (int col = 0; col < 2; col++)
                 {
-                    Console.Write((arr1[row, col]+arr2[row,col])+"\t");
+                    //Console.Write((arr1[row, col]+arr2[row,col])+"\t");
+                    Console.Write(arr2[row, col] + "\t");
                 }
                 Console.WriteLine();
             }
