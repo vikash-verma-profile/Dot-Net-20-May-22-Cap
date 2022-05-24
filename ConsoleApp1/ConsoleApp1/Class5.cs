8bd21b260be06d84242bbe5fc6d0a8de3aa44d80
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-05-24 16:28:06 +0530

    first commit

--- ConsoleApp1/ConsoleApp1/Class5.cs
+++ ConsoleApp1/ConsoleApp1/Class5.cs
@@ -10,7 +10,10 @@ namespace ConsoleApp1
         {
 
             int[,] arr1 = new int[2, 2];
+            int[,] arr2 = new int[2, 2];
+            //int[,] arr3 = new int[2, 2];
 
+            //Input from arr1
             for (int row = 0; row < 2; row++)
             {
                 for (int col = 0; col < 2; col++)
@@ -19,12 +22,23 @@ namespace ConsoleApp1
                     arr1[row, col] = Convert.ToInt32(Console.ReadLine());
                 }
             }
-
+            //Input from arr2
             for (int row = 0; row < 2; row++)
             {
                 for (int col = 0; col < 2; col++)
                 {
-                    Console.Write(arr1[row, col]+"\t");
+                    Console.WriteLine("Please enter the value for arr2[" + row + "," + col + "]: ");
+                    arr2[row, col] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+
+            //Output
+            for (int row = 0; row < 2; row++)
+            {
+                for (int col = 0; col < 2; col++)
+                {
+                    Console.Write((arr1[row, col]+arr2[row,col])+"\t");
                 }
                 Console.WriteLine();
             }
