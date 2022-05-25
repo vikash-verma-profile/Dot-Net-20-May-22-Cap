0c03ca3549be0eff1a7709c5ca854ce88b6442a9
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-05-25 15:35:00 +0530

    first commit

--- ConsoleApp2/ConsoleApp2/Class2.cs
+++ ConsoleApp2/ConsoleApp2/Class2.cs
@@ -10,24 +10,27 @@ namespace ConsoleApp2
         //body
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a choice what you want to perform \n 1.Add \n 2.Sub ");
-            int choice = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter Number 1:");
-            int Number1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter Number 2:");
-            int Number2 = Convert.ToInt32(Console.ReadLine());
-            switch (choice)
-            {
-                case 1:
-                    Sum(Number1,Number2);
-                    break;
-                case 2:
-                    Sub(Number1, Number2);
-                    break;
-                default:
-                    break;
-            }
-            Sum(3,4);
+            //Console.WriteLine("Please enter a choice what you want to perform \n 1.Add \n 2.Sub ");
+            //int choice = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please enter Number 1:");
+            //int Number1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please enter Number 2:");
+            //int Number2 = Convert.ToInt32(Console.ReadLine());
+            //switch (choice)
+            //{
+            //    case 1:
+            //        Sum(Number1,Number2);
+            //        break;
+            //    case 2:
+            //        Sub(Number1, Number2);
+            //        break;
+            //    default:
+            //        break;
+            //}
+            //Sum(3,4);
+
+            string FullName=GetFullName("Vikash", "Verma");
+            Console.WriteLine(FullName);
         }
 
         static void Sum(int a,int b)
@@ -37,6 +40,11 @@ namespace ConsoleApp2
         static void Sub(int a, int b)
         {
             Console.WriteLine(a-b);
+        }
+
+        static string GetFullName(string FirstName,string LastName)
+        {
+            return (FirstName + "-----" + LastName);
         }
     }
 }
