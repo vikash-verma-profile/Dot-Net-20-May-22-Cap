2200b46ec2fd6809cdc89c1de68ab8d2897d00c1
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-05-28 11:14:00 +0530

    first commit

--- ConsoleApp2/ConsoleApp2/Class3.cs
+++ ConsoleApp2/ConsoleApp2/Class3.cs
@@ -20,6 +20,13 @@ namespace ConsoleApp2
             int result;
             sample(10, out result);
             Console.WriteLine("Out value \na=" + result);
+
+            //only able to get one value or variable from the function
+            int result1 = x12();
+
+            //can recive multipe values from a function.
+            int result2,result3,result4;
+            x1(out result2, out result3, out result4);
         }
 
         static void Swap(ref int a, ref int b)
@@ -36,7 +43,7 @@ namespace ConsoleApp2
              s= z + 1;
         }
 
-        static int x()
+        static int x12()
         {
             return 1;
         }
