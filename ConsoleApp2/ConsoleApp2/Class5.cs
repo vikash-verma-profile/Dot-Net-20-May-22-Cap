57ae1242fa20142257a60b6005e1b60320dab3c0
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-05-28 15:33:29 +0530

    first commit

--- ConsoleApp2/ConsoleApp2/Class5.cs
+++ ConsoleApp2/ConsoleApp2/Class5.cs
@@ -33,28 +33,62 @@ namespace ConsoleApp2
         public string Name;
         public int RollNo;
         public string ClassName;
+
+        //get Input from User
+
+        //Methods in a class
+        public void GetData()
+        {
+            Console.WriteLine("Please enter Student Details");
+            Console.WriteLine("Please enter Student Name");
+            Name = Console.ReadLine(); 
+            Console.WriteLine("Please enter Student Roll Number");
+            RollNo = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Please enter Student ClassName");
+            ClassName = Console.ReadLine();
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Student Details are");
+            Console.WriteLine(Name);
+            Console.WriteLine(RollNo);
+            Console.WriteLine(ClassName);
+        }
     }
 
     class Class5
     {
         static void Main(string[] args)
         {
-            sample s1 = new sample();
-            s1.a=6;
+            //sample s1 = new sample();
+            //s1.a=6;
 
-            Console.WriteLine(s1.a);
+            //Console.WriteLine(s1.a);
 
             //<class-name> <object-name>=new <class-Name>()
 
-            student s = new student();
-            s.Name = "Ravi";
-            s.RollNo = 101;
-            s.ClassName = "I";
+            //student s = new student();
+            //s.GetData();
+            //s.PrintData();
+            //student s1 = new student();
+            //s1.GetData();
+            //s1.PrintData();
 
-            Console.WriteLine("Student Details are");
-            Console.WriteLine(s.Name);
-            Console.WriteLine(s.RollNo);
-            Console.WriteLine(s.ClassName);
+            //array of objects
+            student[] studentlist = new student[3];
+            for (int i = 0; i < studentlist.Length; i++)
+            {
+                student s = new student();
+                s.GetData();
+                studentlist[i] = s;
+            }
+
+            for (int i = 0; i < studentlist.Length; i++)
+            {
+                studentlist[i].PrintData();            
+            }
+
         }
     }
 }
