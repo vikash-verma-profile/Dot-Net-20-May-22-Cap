6b73c890db57f123dd38f998e7968d1c41797da5
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-05-30 09:55:34 +0530

    first commit

---
+++ ConsoleApp3/ConsoleApp3/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    //overloading
+    //functions will have same name but they will very in the number of arguments or type of arguments
+    class Class1
+    {
+        public static void sum()
+        {
+            int sum = 1 + 2;
+            Console.WriteLine(sum);
+        }
+        public static void sum(int a,int b)
+        {
+            int sum = a + b;
+            Console.WriteLine(sum);
+        }
+        public static void sum(int a, int b,int c)
+        {
+            int sum = a + b+c;
+            Console.WriteLine(sum);
+        }
+        public static void sum(string a,string b)
+        {
+            int sum = Convert.ToInt32(a) + Convert.ToInt32(b);
+            Console.WriteLine(sum);
+        }
+        public static void Main()
+        {
+            sum();
+            sum(1, 2, 3);
+            sum(1, 2);
+            sum("1", "2");
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    //abstract
+
+    //abstract class cannot have an object.
+    //abstarct class can be extendend by any derived class
+    //it can have constructor and destructors
+    //it cannot sipport multiple inheritance
+    //it cannot be static
+    abstract class Sample
+    {
+        public abstract void print();
+    }
+
+    class Sample2:Sample
+    {
+        public override void print()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
