1190ccf91e42fdd19689e27368be84919222ed7c
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-05-31 14:39:43 +0530

    first commit

---
+++ DBMS/SQLQuery1.sql
@@ -0,0 +1,9 @@
+Create database SampleDb2;
+Drop database SampleDb2;
+--Create table
+/*
+create table <TABLE NAME>(<columns> data-type)
+*/
+create table sample(id int,name varchar(200));
+
+select * from sample;
