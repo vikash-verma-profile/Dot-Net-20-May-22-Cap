cdf009e8e5f61e2a4cda2f3c63e13ab12952d9cc
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-05-31 15:11:27 +0530

    first commit

--- DBMS/SQLQuery1.sql
+++ DBMS/SQLQuery1.sql
@@ -6,4 +6,13 @@ create table <TABLE NAME>(<columns> data-type)
 */
 create table sample(id int,name varchar(200));
 
-select * from sample;
+select * from sample where id=3;
+
+insert into sample values(1,'Vikash 1');
+insert into sample values(2,'Vikash 2');
+insert into sample values(3,'Vikash 3');
+insert into sample values(4,'Vikash 4');
+
+delete  from sample where id=1;
+
+update  sample set name='rohit' where id=2;
