8e6179766c73c50c5db5b4789b9d8e92729d7e2e
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-05-31 18:05:03 +0530

    first commit

--- DBMS/SQLQuery1.sql
+++ DBMS/SQLQuery1.sql
@@ -1,5 +1,6 @@
 Create database SampleDb2;
 Drop database SampleDb2;
+use SampleDb1
 --Create table
 /*
 create table <TABLE NAME>(<columns> data-type)
@@ -21,14 +22,18 @@ update  sample set name='rohit' where id=2;
 
 create table student(id int,rollno int,FirstName varchar(20),LastName varchar(20),Gender varchar(10));
 
-select * from student;
+select Gender from student group by Gender having count(*)>2;
 
+select FirstName from student group by FirstName having count(*)>1;
 
 insert into student values(1,'101','Vikash',' 1','Male');
 insert into student values(2,'102','Rohit','Kumar','Male');
 insert into student values(3,'103','Viyan','Soni','Male');
 insert into student values(4,'104','Anjali','Soni','FeMale');
 insert into student values(5,'105','Shivani','Verma','FeMale');
+insert into student values(6,'106','Raj','Kumar','Other');
+
+insert into student values(7,'107','Vikash',' 1','Male');
 
 select * from student where Gender='Female';
 
@@ -55,3 +60,29 @@ select  * from student where FirstName like 'V%'
 select  * from student where FirstName like 'a%'
 
 select CONCAT(FirstName,' ',LastName) as FullName  from student
+
+select * from student where rollno between 101 and 105
+
+
+create table employee (id int,name varchar(200),departmentid int)
+create table department(id int,department varchar(20))
+insert into employee values(1,'Vikash',1)
+insert into employee values(2,'Rohit',2)
+insert into employee values(3,'Rahul',1)
+insert into employee values(4,'Rahul',3)
+
+insert into department values(1,'IT')
+insert into department values(2,'Sales')
+insert into department values(4,'Admin')
+
+select * from employee
+select * from department
+
+select emp.name,dep.department from employee as emp inner join department as dep on emp.departmentid=dep.id
+
+select emp.name,dep.department from employee as emp left join department as dep on emp.departmentid=dep.id
+
+select emp.name,dep.department from employee as emp right join department as dep on emp.departmentid=dep.id
+
+select * from employee as emp inner join department as dep on emp.departmentid=dep.id;
+
