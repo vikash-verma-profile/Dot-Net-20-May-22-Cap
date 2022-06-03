d70ae1e499a5d6531bc905e06b5ae1737314a5a5
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-06-03 17:56:53 +0530

    first commit

--- ConsoleApp3/ConsoleApp3/Class11.cs
+++ ConsoleApp3/ConsoleApp3/Class11.cs
@@ -9,7 +9,30 @@ namespace ConsoleApp3
     {
         public static void Main()
         {
+            try
+            {
+                int a = 5, b = 0;
+               // int c = a / b;
+                int[] ar = new int[5];
+                //ar[5] = 9;
+                //Console.WriteLine(c);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Index out---Some error occured");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Some error occured");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
 
+            finally{
+                Console.WriteLine("I am coming from finally");
+            }
         }
     }
 }
