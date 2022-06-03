93753b2722f80b95331f4ef14e44cbf408e9150d
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-06-03 16:48:13 +0530

    first commit

--- ConsoleApp3/ConsoleApp3/Class10.cs
+++ ConsoleApp3/ConsoleApp3/Class10.cs
@@ -1,52 +1,48 @@
 ﻿using System;
-using System.Collections;
+using System.Collections.Generic;
+using System.Text;
 
-//Non-Genric collections
 namespace ConsoleApp3
 {
-
-    //arraylist
-    class Class9
+    //Generic collections
+    //specific type
+    class Class10
     {
-        public static void Main10()
+        public static void Main()
         {
 
-            //ArrayList al = new ArrayList();
-            //string str = "Vikash";
-            //int x = 7;
-            //DateTime d = DateTime.Now;
-            //al.Add(str);
-            //al.Add(x);
-            //al.Add(d);
-            //al.Remove(d);
+            List<int> lst = new List<int>();
+            lst.Add(100);
+            lst.Add(101);
+            lst.Add(102);
+            lst.Add(103);
+            lst.Add(104);
 
-            //foreach (var item in al)
-            //{
-            //    Console.WriteLine(item);
-            //}
-
-            Hashtable ht = new Hashtable();
-            ht.Add("ora","oracle");
-            ht.Add("vb", "vb.Net");
-            ht.Add("cs", "cs.net");
-            ht.Add("asp", "asp.net");
-
-            //DictionaryEntry is a class whose object represents the data in combination of key and value
-            foreach (DictionaryEntry item in ht)
+            foreach (var item in lst)
             {
-                Console.WriteLine(item.Key+"===>"+item.Value);
+                Console.WriteLine(item);
             }
 
-            Console.WriteLine("================");
+            Dictionary<int, string> dct = new Dictionary<int, string>();
+            dct.Add(1,"cs.net");
+            dct.Add(2, "vb.net");
+            dct.Add(3, "asp.net");
+            dct.Add(4, "fs.net");
+
+            foreach (KeyValuePair<int,string> item in dct)
+            {
+                Console.WriteLine(item.Key+"=========>"+item.Value);
+            }
+
             //sortedlist
             //it is a class that has the combination of arraylist and hashtable
-            SortedList sl = new SortedList();
+            SortedList<string,string> sl = new SortedList<string,string>();
             sl.Add("ora", "oracle");
             sl.Add("vb", "vb.Net");
             sl.Add("cs", "cs.net");
             sl.Add("asp", "asp.net");
 
-            foreach (DictionaryEntry item in sl)
+            foreach (KeyValuePair<string, string> item in sl)
             {
                 Console.WriteLine(item.Key + "===>" + item.Value);
             }
@@ -54,7 +50,7 @@ namespace ConsoleApp3
             //stack
             //LIFO
             Console.WriteLine("================");
-            Stack stk = new Stack();
+            Stack<string> stk = new Stack<string>();
             stk.Push("cs.net");
             stk.Push("vb.net");
             stk.Push("asp.net");
@@ -68,7 +64,7 @@ namespace ConsoleApp3
             Console.WriteLine("================");
             //queue
             //FIFO
-            Queue q = new Queue();
+            Queue<string> q = new Queue<string>();
             q.Enqueue("cs.net");
             q.Enqueue("vb.net");
             q.Enqueue("asp.net");
