cdc47cc80b10cd4742e9ca61eed1a0561cf4e5a0
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-06-08 11:39:06 +0530

    first commit

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public string Get()
+        {
+            return "Vikash Verma";
+        }
+    }
+}
