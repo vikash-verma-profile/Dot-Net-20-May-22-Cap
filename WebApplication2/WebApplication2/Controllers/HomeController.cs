d84bd7e8f995d2507f86c70490175810db1d9281
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-06-09 09:51:20 +0530

    first commit

--- WebApplication2/WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -28,5 +28,35 @@ namespace WebApplication2.Controllers
             db.SaveChanges();
             return "Success";
         }
+        [HttpPut]
+        public string Put([FromBody] Tblsample tblsample)
+        {
+            SampleDBContext db = new SampleDBContext();
+            var tblsampleObj = db.Tblsamples.Where(x => x.Id == tblsample.Id);
+            if (tblsampleObj != null)
+            {
+                db.Tblsamples.Update(tblsample);
+                db.SaveChanges();
+                return "Success";
+            }
+          
+            return "Fail";
+        }
+
+        [HttpDelete]
+        public string Delete([FromBody] int Id)
+        {
+            SampleDBContext db = new SampleDBContext();
+            var tblsampleObj = db.Tblsamples.Where(x => x.Id ==Id).FirstOrDefault();
+            if (tblsampleObj != null)
+            {
+                db.Tblsamples.Remove(tblsampleObj);
+                db.SaveChanges();
+                return "Success";
+            }
+
+            return "Fail";
+        }
+
     }
 }
