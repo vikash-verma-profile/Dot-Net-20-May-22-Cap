02f5fca77fb9dfe99196df6ad8b2fd713807dd40
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-06-10 16:48:45 +0530

    first commit

--- Angular-Labs/first-sample-project/src/app/app.component.ts
+++ Angular-Labs/first-sample-project/src/app/app.component.ts
@@ -8,6 +8,18 @@ import { Customer } from './app.model';
 })
 export class AppComponent {
   title = 'first-sample-project';
-  myname='vikash Verma'
-  CustomerModel:Customer=new Customer();
+  myname = 'vikash Verma'
+  CustomerModel: Customer = new Customer();
+  CustmerModels: Array<Customer> = new Array<Customer>();
+  AddCustomer() {
+    this.CustmerModels.push(this.CustomerModel);
+    this.CustomerModel = new Customer();
+  }
+  EditCustomer(input: Customer) {
+    console.log(input);
+  }
+  DeleteCustomer(input: Customer) {
+    console.log(input);
+  }
+
 }
