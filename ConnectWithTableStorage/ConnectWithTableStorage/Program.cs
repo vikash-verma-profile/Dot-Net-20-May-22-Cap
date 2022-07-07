3a2a9da73ac0dbdcc1161f0455c2b23207e97e4a
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-07-07 10:34:00 +0530

    first commit

---
+++ ConnectWithTableStorage/ConnectWithTableStorage/Program.cs
@@ -0,0 +1,20 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using System;
+
+namespace ConnectWithTableStorage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var client = new TableClient(new Uri("https://levelupsolutions.table.core.windows.net"), "tblSample",new TableSharedKeyCredential("levelupsolutions", "o1BI0aWmaCWS7/pp7OiljEQdRORQA/rZA474QOW1X56JUz+CEPOXymF1nHnEaZ3dLIYwfewPmAcQ+AStqmDWVQ=="));
+            Pageable<TableEntity> queryableTable = client.Query<TableEntity>();
+            foreach (TableEntity item in queryableTable)
+            {
+                Console.WriteLine(item.GetString("Name"));
+            }
+        }
+    }
+}
