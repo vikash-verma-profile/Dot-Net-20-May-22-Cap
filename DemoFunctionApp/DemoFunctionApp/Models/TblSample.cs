4051284fbba771a0702031d6a49eb48eee16e19d
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-07-13 14:34:23 +0530

    added

---
+++ DemoFunctionApp/DemoFunctionApp/Models/TblSample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DemoFunctionApp.Models
+{
+    public partial class TblSample
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ DemoFunctionApp/DemoFunctionApp/Models/demodbContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace DemoFunctionApp.Models
+{
+    public partial class demodbContext : DbContext
+    {
+        public demodbContext()
+        {
+        }
+
+        public demodbContext(DbContextOptions<demodbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblSample> TblSamples { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=tcp:sample001.database.windows.net,1433;Initial Catalog=demodb;Persist Security Info=False;User ID=sample;Password=Levelupsolutions@007;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<TblSample>(entity =>
+            {
+                entity.ToTable("tblSample");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Text).HasMaxLength(200);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
